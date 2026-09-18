d3116e74d49b82f84ca018c83a8cb84c2faccbf9
Ivan Wokfa <ivanwokfa@icloud.com>
2026-09-18 20:44:13 +0200

    Add frontend and admin system

--- src/main/java/com/budgetapp/budget_app/config/SecurityConfig.java
+++ src/main/java/com/budgetapp/budget_app/config/SecurityConfig.java
@@ -25,6 +25,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/", "/index.html", "/**.html", "/**.css", "/**.js").permitAll()
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers("/admin/register").permitAll()
                         .anyRequest().authenticated()
